1874cc6b3745b6e33599dee7ecab34ba25a9ca8e
kod-python <kelvinlik0245@gmail.com>
2024-06-27 10:40:15 +0000

    swagger classbase

---
+++ swag/swagapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Product {self.id} - {self.name}"
