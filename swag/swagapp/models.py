7dfb739f29272775e157f7b2f2a77b9412237aa4
kod-python <kelvinlik0245@gmail.com>
2024-07-05 18:14:29 +0000

    reload add

--- swag/swagapp/models.py
+++ swag/swagapp/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(default=False, blank=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
     quantity = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
