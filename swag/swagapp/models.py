6c9df988788969ec554493949d9a922cd65c5c64
kod-python <kelvinlik0245@gmail.com>
2024-07-05 17:17:11 +0000

    image adde

--- swag/swagapp/models.py
+++ swag/swagapp/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(default=False, blank=True)
     quantity = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- swag/swagapp/urls.py
+++ swag/swagapp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
