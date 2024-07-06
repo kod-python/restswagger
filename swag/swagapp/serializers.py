850c4b14be9bb4e54899a5a588c049fe8d1d857c
kod-python <kelvinlik0245@gmail.com>
2024-07-06 15:54:38 +0000

    new start

--- swag/swagapp/serializers.py
+++ swag/swagapp/serializers.py
@@ -5,3 +5,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'image', 'quantity', 'created', 'updated']
+         
+        def get_photo_url(self,obj):
+             request = self.context.get('request')
+             photo_url = obj.fingerprint.url
+             return request.build.absolute_uri(photo_url)
