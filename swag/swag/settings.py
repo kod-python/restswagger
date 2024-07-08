01ff1b079653c79e512ea085512b890fc08f96e9
kod-python <kelvinlik0245@gmail.com>
2024-07-08 17:42:21 +0000

    nap commit

--- swag/swag/settings.py
+++ swag/swag/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nr)n)spg$%yxt-woytwiol2z)&#m+@@x5@(*p%bhw07u%@$+@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'kodviper.pythonanywhere.com']
 
 
 # Application definition
